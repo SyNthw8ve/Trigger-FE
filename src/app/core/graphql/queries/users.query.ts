5bdf3694f5ed6bcbef592d5915627d1d74db0186
SyNthw8ve <nunofmcarrico@gmail.com>
2020-09-01 16:46:28 +0100

    Apollo integration concluded

--- src/app/core/graphql/queries/users.query.ts
+++ src/app/core/graphql/queries/users.query.ts
@@ -1,32 +1,33 @@
 import gql from 'graphql-tag';
 
-export const getUsers = gql`
+export const getUsers = 
+gql`
     query {
-  getUsers {
-    name
-    email
-    password
-    availability {
-      hours
+        getUsers {
+            name
+            email
+            password
+            availability {
+                hours
+            }
+            projects {
+                title
+            }
+            institution {
+                description
+            }
+            hardSkills {
+                name
+            }
+            languages {
+                name
+            }
+            softSkills {
+                score
+                softskillId {
+                    name
+                }
+            }
+        }
     }
-    projects {
-      title
-    }
-    institution {
-      description
-    }
-    hardSkills {
-      name
-    }
-    languages {
-      name
-    }
-    softSkills {
-      score
-      softskillId {
-        name
-      }
-    }
-  }
-}
 `
--- src/app/modules/home/home.page.ts
+++ src/app/modules/home/home.page.ts
@@ -1,27 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Apollo } from 'apollo-angular';
-import { Subscription } from 'rxjs';
-import { getUsers } from '../../core/graphql/queries/users.query';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage {
 
-  private querySubscription: Subscription;
-  users = [];
+  constructor() { }
 
-  constructor(private apollo: Apollo) { }
-
-  ngOnInit() {
-
-    this.querySubscription = this.apollo.watchQuery<any>({ query: getUsers }).valueChanges
-      .subscribe(({ data, loading }) => {
-       
-        this.users = data.getUsers;
-        console.log(this.users)
-      });
-  }
 }
