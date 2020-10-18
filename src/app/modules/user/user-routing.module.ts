3cb86b301481c31b406a1b5e8934e1ac93472e2e
SyNthw8ve <nunofmcarrico@gmail.com>
2020-10-18 20:04:23 +0100

    Login Guard

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { LoginGuard } from 'src/app/core/guards/login.guard';
 
 const routes: Routes = [
   {
     path: 'profile',
     component: UserProfileComponent,
+    canActivate: [LoginGuard]
   }
 ];
 
